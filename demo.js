42b8203b9df0bc673475ef3b6ff1be8a41385021
reyalmonte809 <reyalmonte89@gmail.com>
2024-04-23 15:00:55 -0400

    exercise

---
+++ demo.js
@@ -0,0 +1,15 @@
+var name = "Brittany";
+console.log( "Your name is " + name);
+
+var price, discount;
+price = 22.50;
+discount = price * .1;
+console.log(discount)
+
+"use strict";
+
+var phrase1, phrase2, wholePhrase;
+phrase1= "Learning to program ";
+phrase2= "is fun!";
+wholePhrase= phrase1 + phrase2 
+console.log(wholePhrase)
---
+++ exercise.js
@@ -0,0 +1,23 @@
+var id, firstName, middleName, lastName, gender, DOB, DL, auto
+id= "MichaelBJordan ";
+firstName= "Michael ";
+middleName= "B ";
+lastName= "Jordan ";
+gender= "Male ";
+DOB= "02/09/1987 "
+DL= "J126-234-09-478-0 "
+auto= "12345678-9 "
+all= id + firstName + middleName + lastName + gender + DOB + DL + auto
+console.log(all)
+
+
+1 var abstract, break, Char, debugger, double, try, finally, goto, in, null;
+2 array, date, eval, function, math, NaN, name, string, object, toString;
+3 alert,	all,	anchor,	anchors, area,	assign,	blur,	button, checkbox,	clearInterval
+4onblur,	onclick,	onerror,	onfocus, onkeydown,	onkeypress,	
+
+var firstName1, lastName2, city, state
+firstName1= "Reynerio"
+lastName2= "Almonte Contreras"
+city= "Santo Domingo"
+state= "Dominican Republic"
