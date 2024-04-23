520fd7c6fb6f07ed9ce53af281f9914f3d52727e
reyalmonte809 <reyalmonte89@gmail.com>
2024-04-23 15:59:46 -0400

    logic

---
+++ demotype.js
@@ -0,0 +1,9 @@
+text = "ABC"
+value = 123
+when = new Date()
+
+console.log(typeof text) //string
+console.log(typeof value) // number
+console.log(typeof when) // object
+
+console.log(text + value+ when)
