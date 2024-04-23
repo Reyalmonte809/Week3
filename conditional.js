06aeaf0ce28fbbc470e4c5b7982037f45650d45a
reyalmonte809 <reyalmonte89@gmail.com>
2024-04-23 10:52:33 -0400

    first commit

---
+++ conditional.js
@@ -0,0 +1,61 @@
+const age = 17;
+let can_vote = undefined;
+
+if(age >= 18){
+    can_vote = true;
+}else{
+     can_vote = false;
+}
+
+
+console.log(can_vote);
+
+const aged = 55;
+let can_voted = undefined;
+
+if(aged >= 18 && aged <= 100){
+    can_voted = true;
+}else{
+     can_voted = false;
+}
+
+
+console.log(can_voted);
+
+// petCount = 4
+// MAX_PETS = 3
+// allowed = undefined
+
+// if(petCount <= MAX_PETS){
+//     allowed = true;
+// }else{
+//     allowed = false;
+// }
+// console.log(allowed);
+
+// petCount = 2
+// MAX_PETS = 3
+// allowed = undefined
+
+// if(petCount <= MAX_PETS){
+//     allowed = true;
+// }else{
+//     allowed = false;
+// }
+// console.log(allowed);
+
+let petCount = 2
+let MAX_PETS = 3
+let allow = undefined
+let cashOnHand = 1000
+let DEPOSIT_AMOUNT = 1200
+let isWithinLimit = petCount <= MAX_PETS;
+let hasEnoughCash = cashOnHand >+ DEPOSIT_AMOUNT
+
+if(isWithinLimit && hasEnoughCash){
+    allow = true;
+}else{
+    allow = false;
+}
+
+console.log(allow)
---
+++ main.js
@@ -0,0 +1,4 @@
+console.log(6*7)
+myName = "Reynerio"
+console.log("Hello " + myName)
+console.log("Hello" + myName)
