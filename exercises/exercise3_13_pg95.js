12c67838b43198c3f710cfcc1b1ca6000f401dc2
reyalmonte809 <reyalmonte89@gmail.com>
2024-04-25 14:20:34 -0400

    game

---
+++ exercises/exercise3_13_pg95.js
@@ -0,0 +1,20 @@
+//min_max.js
+
+var a = "65"
+var b = "56"
+var c = "46"
+
+value = 65
+minMax=""
+
+if(value <= 46){
+   minMax ="lowest" 
+}else if (value <= 56){
+    minMax = "middle"
+}else{
+    minMax = "highest"
+}
+
+console.log(minMax)
+
+
